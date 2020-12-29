534f58f03c03363e039fb68cfbfcff996fb3ba07
jingelman <engelbrektsson37@gmail.com>
2020-12-29 11:45:23 +0100

    missing file

--- main.cpp
+++ main.cpp
@@ -1,111 +1,124 @@
+
+#include <Header/QuadTree.h>
+#include <Header/QuadTreeData.h>
+
 #include <SFML/Graphics.hpp>
 
-#include <stdlib.h>     /* srand, rand */
-#include <time.h>
-//#include <math.h>       /* pow */
+#include <string> // std::string, std::to_string
 
-#include "QuadTree.h"
+constexpr uint C_FRAMERATE = 60;
 
-constexpr int C_NROFSHAPES = 512;
-constexpr int C_FRAMERATE = 60;
+#ifdef _DEBUG
+void DrawDebugQuads(sf::RenderWindow& window, QuadTree& qTree);
+#endif
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(500, 500), "SFML works!");
+	sf::RenderWindow window(sf::VideoMode(400, 400), "SFML works!");
 	window.setFramerateLimit(C_FRAMERATE);
 
+	g_nodeData = new QuadTreeData(window.getSize());
+
 	sf::Clock clock;
-	srand(time(NULL));
 
-	
-	sf::CircleShape shapes[C_NROFSHAPES];
-	for (int i = 0; i < C_NROFSHAPES; i++)
+	sf::Text ms;
+	sf::Font font;
+	if (!font.loadFromFile("C:/Windows/Fonts/arial.ttf"))
 	{
-		sf::CircleShape shape(2.f);
-		shape.setPosition(rand() % 500, rand() % 500);
-		shapes[i] = shape;
+		std::cout << "Error: Coulnt load font";
 	}
-
-	QuadTree quadTree(window.getSize());
-
-	for (int i = 0; i < C_NROFSHAPES; i++)
-	{
-		quadTree.insertNode(shapes[i]);
-	}
-
-	int s = 0;
-	std::vector<sf::CircleShape*> sshapes;
-	quadTree.CollectShapes(s, sshapes);
-
-	std::vector<sf::Vector2u> lines;
-	quadTree.CollectQuads(lines);
-
-
-	sf::Vertex vertex;
-	vertex.position = sf::Vector2f(10.f, 50.f);
-	vertex.color = sf::Color::Red;
-	vertex.texCoords = sf::Vector2f(100.f, 100.f);
-
-	//sf::VertexArray line(sf::Lines, lines.size());
-	//for (int i = 0; i < lines.size(); i++)
-		//line[i].position = sf::Vector2f(lines[i]);
-	//line[1].position = sf::Vector2f(100.f, 100.f);
-
-	//sf::RectangleShape vetricalLine(sf::Vector2f(500.f))
-
-	//sf::CircleShape shape(10.f);
-	//shape.setFillColor(sf::Color::Green);
-	//const sf::Vector2f pos = shape.getPosition();
-	//shape.setPosition(pos.x + 10.0f, pos.y + 10.0f);
+	ms.setFont(font);
+	ms.setCharacterSize(24); // in pixels, not points!
+	ms.setFillColor(sf::Color::Red);
+	ms.setStyle(sf::Text::Bold | sf::Text::Underlined);
 
 	while (window.isOpen())
 	{
-
+		//########### EVENT ############
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
+		//############ END ############
 
+		//######## SIMULATION #########
+		//float t = clock.getElapsedTime().asSeconds();
+		//std::cout << "Time1: " << t << std::endl;
+		g_nodeData->UpdateParticles(g_nodeData, window.getSize(), 1.f / 60.f);
+
+		QuadTree quadTree(window.getSize());
+
+		quadTree.InsertNodes(g_nodeData->m_circleShapes, g_nodeData->m_currentParticleSize);
+
+		quadTree.NewCheckCollision(g_nodeData->m_circleShapes);
+
+		//std::vector<uint> collisionOut;
+		//quadTree.CheckCollisions(g_nodeData->m_circleShapes, g_nodeData->m_currentParticleSize, collisionOut);
+		//sort(collisionOut.begin(), collisionOut.end());
+		//collisionOut.erase(unique(collisionOut.begin(), collisionOut.end()), collisionOut.end());
+		//############ END ############
+
+		//########## RENDER ###########
 		window.clear();
-		/*
-		sf::Vector2f ln = sf::Vector2f(lines[3]);
-		sf::RectangleShape line(ln);
-		window.draw(line);
-		*/
-		
-		for (int i = 0; i < lines.size(); i += 2)
-		{
-			sf::VertexArray line(sf::Lines, 8);
 
-			sf::Vector2f p0 = sf::Vector2f(lines[i]);
-			sf::Vector2f p3 = sf::Vector2f(lines[i + 1]);
-			sf::Vector2f p1 = sf::Vector2f(lines[i + 1].x, lines[i].y);
-			sf::Vector2f p2 = sf::Vector2f(lines[i].x, lines[i + 1].y);
-			
-			line[0].position = p0;
-			line[1].position = p1;
-			line[2].position = p0;
-			line[3].position = p2;
-			line[4].position = p1;
-			line[5].position = p3;
-			line[6].position = p2;
-			line[7].position = p3;
+#ifdef _DEBUG
+		DrawDebugQuads(window, quadTree);
+#endif
 
-			window.draw(line);
+		{ // Draw particles
+			int size = g_nodeData->m_currentParticleSize;
+			sf::CircleShape* s = g_nodeData->m_circleShapes;
+			for (int i = 0; i < size; i++)
+			{
+				window.draw(s[i]);
+				s[i].setFillColor(sf::Color::White);
+			} /*
+			for (const uint c : collisionOut)
+			{
+				sf::CircleShape temp = s[c];
+				temp.setFillColor(sf::Color(255, 0, 0));
+				window.draw(temp);
+			}*/
 		}
-		//window.draw(line);
-		for (int i = 0; i < C_NROFSHAPES; i++)
-		{
-			window.draw(shapes[i]);
-			
-		}
-		//window.draw(shapes, 25);
-		//window.draw(shapes[0]);
-		//window.draw(shape);
+		//#ifdef _DEBUG
+		sf::Time elapsed = clock.restart();
+		std::string elapsedMs = std::to_string(elapsed.asMilliseconds());
+		ms.setString("ms: " + elapsedMs);
+		window.draw(ms);
+		//#endif
 		window.display();
+		//############ END ############
 	}
 
 	return 0;
 }
+
+#ifdef _DEBUG
+void DrawDebugQuads(sf::RenderWindow& window, QuadTree& quadTree)
+{
+	std::vector<sf::Vector2u> lines;
+	quadTree.GetQuads(lines);
+	for (size_t i = 0; i < lines.size(); i += 2)
+	{
+		sf::VertexArray line(sf::Lines, 8);
+
+		sf::Vector2f p0 = sf::Vector2f(lines[i]);
+		sf::Vector2f p3 = sf::Vector2f(lines[i + 1]);
+		sf::Vector2f p1 = sf::Vector2f(lines[i + 1].x, lines[i].y);
+		sf::Vector2f p2 = sf::Vector2f(lines[i].x, lines[i + 1].y);
+
+		line[0].position = p0;
+		line[1].position = p1;
+		line[2].position = p0;
+		line[3].position = p2;
+		line[4].position = p1;
+		line[5].position = p3;
+		line[6].position = p2;
+		line[7].position = p3;
+
+		window.draw(line);
+	}
+}
+#endif
